9d3a74f1007cbc99d7f3105f5b9581ae8b5e9bac
kupessa <loris.kupessa@gmail.com>
2021-08-14 00:34:19 +0100

    Primeiro commit

---
+++ src/main/java/org/chandona/peoplezero/PeoplezeroApplication.java
@@ -0,0 +1,13 @@
+package org.chandona.peoplezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplezeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplezeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chandona/peoplezero/PeoplezeroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandona.peoplezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplezeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
